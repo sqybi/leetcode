134381adf031576f68ef3a3e5c7069dca478863d
sqybi <sqybi@126.com>
2014-07-29 16:44:20 +0800

    Linked List Cycle

---
+++ LeetCode/linked-list-cycle/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	bool hasCycle(ListNode *head) {
+		if (head == NULL || head->next == NULL)
+		{
+			return false;
+		}
+		auto slow = head;
+		auto fast = head->next;
+		while (slow != fast && fast->next != NULL && fast->next->next != NULL)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		return slow == fast;
+	}
+};
