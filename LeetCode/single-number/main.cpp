ad06302ee0abe944fce0b25a9a7b7c39587008d4
sqybi <sqybi@126.com>
2014-07-31 16:48:17 +0800

    Single Number

---
+++ LeetCode/single-number/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	int singleNumber(int A[], int n) {
+		int result = 0;
+		for (int i = 0; i < n; i++)
+		{
+			result ^= A[i];
+		}
+		return result;
+	}
+};
+
+int main()
+{
+	return 0;
+}
