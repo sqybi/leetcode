3609ccac298afd839b321186e913aa15352dfb67
sqybi <sqybi@126.com>
2014-08-11 11:52:12 +0800

    Remove Duplicates from Sorted List II

--- LeetCode/remove-duplicates-from-sorted-list-ii/main.cpp
+++ LeetCode/remove-duplicates-from-sorted-list-ii/main.cpp
@@ -13,19 +13,70 @@ public:
     ListNode *deleteDuplicates(ListNode *head) {
         ListNode *dup = NULL;
         auto node = head;
+        head = NULL;
+        ListNode *tail = NULL;
+        bool flag = false;
         while (node != NULL)
         {
             auto next = node->next;
-            if (dup == NULL || node->val != dup->val)
+            if (dup == NULL)
             {
                 dup = node;
+                flag = true;
             }
             else
             {
-                delete node;
-                dup->next = next;
+                if (dup->val == node->val)
+                {
+                    flag = false;
+                    delete dup;
+                    dup = node;
+                }
+                else
+                {
+                    if (flag)
+                    {
+                        if (head == NULL)
+                        {
+                            head = dup;
+                            tail = dup;
+                        }
+                        else
+                        {
+                            tail->next = dup;
+                            tail = tail->next;
+                        }
+                    }
+                    else
+                    {
+                        delete dup;
+                    }
+                    dup = node;
+                    flag = true;
+                }
             }
             node = next;
+        }
+        if (flag)
+        {
+            if (head == NULL)
+            {
+                head = dup;
+                tail = dup;
+            }
+            else
+            {
+                tail->next = dup;
+                tail = tail->next;
+            }
+        }
+        else
+        {
+            delete dup;
+        }
+        if (head != NULL)
+        {
+            tail->next = NULL;
         }
         return head;
     }
@@ -33,5 +84,10 @@ public:
 
 int main()
 {
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(2);
+    Solution solution;
+    solution.deleteDuplicates(head);
     return 0;
 }
