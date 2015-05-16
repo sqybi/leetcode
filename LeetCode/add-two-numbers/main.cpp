3e2dd3b03d0b200de484bb552588f980d263f170
sqybi <sqybi@126.com>
2015-05-16 22:03:27 +0800

    add-two-numbers

---
+++ LeetCode/add-two-numbers/main.cpp
@@ -0,0 +1,64 @@
+#include <cstdlib>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode* head = nullptr;
+		ListNode* tail;
+		int c = 0;
+		while (l1 != nullptr && l2 != nullptr)
+		{
+			c += l1->val + l2->val;
+			if (head == nullptr)
+			{
+				head = new ListNode(c % 10);
+				tail = head;
+			}
+			else
+			{
+				tail->next = new ListNode(c % 10);
+				tail = tail->next;
+			}
+			c /= 10;
+			l1 = l1->next;
+			l2 = l2->next;
+		}
+
+		if (l1 == nullptr)
+		{
+			l1 = l2;
+		}
+
+		while (l1 != nullptr)
+		{
+			c += l1->val;
+			if (head == nullptr)
+			{
+				head = new ListNode(c % 10);
+				tail = head;
+			}
+			else
+			{
+				tail->next = new ListNode(c % 10);
+				tail = tail->next;
+			}
+			c /= 10;
+			l1 = l1->next;
+		}
+
+		if (c != 0)
+		{
+			tail->next = new ListNode(c);
+		}
+
+		return head;
+	}
+};
